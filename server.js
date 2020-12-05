e7ad0170a7bdf6c4df92fa207b170c306ad55a66
Matmarss <martin.silvestre.fr@gmail.com>
2020-12-05 19:46:33 +0100

    [UPD] deployement

--- server.js
+++ server.js
@@ -5,11 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/NewRencontre-frontend'));
+app.use(express.static(__dirname + '/dist/new-rencontre-frontend'));
 
 app.get('/*', function(req,res) {
-
-  res.sendFile(path.join(__dirname+'/dist/NewRencontre-frontend/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/new-rencontre-frontend/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
